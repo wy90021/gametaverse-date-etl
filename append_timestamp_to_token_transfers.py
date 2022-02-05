450ec39f822445ffd5ce71010a832cd0c1f6fd13
wy90021 <wy90021@gmail.com>
2022-02-04 19:38:12 -0800

    update

---
+++ append_timestamp_to_token_transfers.py
@@ -0,0 +1,60 @@
+import csv
+from email import header
+import sys
+import json
+
+csv.field_size_limit(sys.maxsize)
+
+rent_address = "0xe9e092e46a75d192d9d7d3942f11f116fd2f7ca9"
+withdrawl_final_address = "0x94019518f82762bb94280211d19d4ac025d98583"
+generate_shark_address = "0x1f7acc330fe462a9468aa47ecdb543787577e1e7"
+auction_address = "0xd78be0b93a3c9d1a9323bca03184accf1a57e548"
+
+in_game_address = set([
+    rent_address,
+    withdrawl_final_address,
+    generate_shark_address,
+    auction_address,
+])
+
+blocks_file_path = "blocks.csv"
+in_game_token_transfers_file_path = "in-game-token-transfers.csv"
+in_game_token_transfers_with_timestamp_file_path = "in-game-token-transfers-with-timestamp.csv"
+block_timestamp_dict = {}
+
+daily_transfers_csv_header = ["token_address", "from_address", "to_address", "value", "transaction_hash", "log_index", "block_number"]
+
+def append_timestamp_to_token_transfers():
+    count = 0
+    output_file = open("in-game-logs.csv", "w")
+    data_writer = csv.writer(output_file)
+    in_game_transaction_hashes = set()
+    with open(blocks_file_path, "r") as blocks_file:
+        data_reader = csv.reader(blocks_file)
+        next(data_reader)
+        for row in data_reader:
+            block_number = row[0]
+            timestamp = row[16]
+            block_timestamp_dict[block_number] = timestamp
+
+    with open(in_game_token_transfers_file_path, "r") as in_game_token_transfers_file:
+        with open(in_game_token_transfers_with_timestamp_file_path, "w") as in_game_token_transfers_with_timestamp_file:
+            data_reader = csv.reader(in_game_token_transfers_file)
+            data_writer = csv.writer(in_game_token_transfers_with_timestamp_file, lineterminator='\n')
+            all = []
+            row = next(data_reader)
+            row.append('timestamp')
+            all.append(row)
+
+            for row in data_reader:
+                block_number = row[6]
+                timestamp = block_timestamp_dict[block_number]
+                row.append(timestamp)
+                all.append(row)
+            data_writer.writerows(all)
+
+def main():
+    append_timestamp_to_token_transfers()
+
+if __name__ == "__main__":
+    main()
---
+++ update_per_user_join_time_json.py
@@ -0,0 +1,75 @@
+import csv
+from email import header
+import sys
+import json
+
+csv.field_size_limit(sys.maxsize)
+
+rent_address = "0xe9e092e46a75d192d9d7d3942f11f116fd2f7ca9"
+withdrawl_final_address = "0x94019518f82762bb94280211d19d4ac025d98583"
+generate_shark_address = "0x1f7acc330fe462a9468aa47ecdb543787577e1e7"
+auction_address = "0xd78be0b93a3c9d1a9323bca03184accf1a57e548"
+
+in_game_address = set([
+    rent_address,
+    withdrawl_final_address,
+    generate_shark_address,
+    auction_address,
+])
+
+{
+    "address": {
+            "timestamp": 1666666,
+            "transaction_hash": "xxxxxxx"
+    }
+}
+
+daily_transfers_with_timestamp_file_path = "in-game-token-transfers-with-timestamp.csv"
+per_user_join_time_json_file_path = "../per-user-join-time.json"
+per_user_join_time = {}
+
+daily_transfers_csv_header = ["token_address", "from_address", "to_address", "value", "transaction_hash", "log_index", "block_number", "timestamp"]
+
+def add_new_user_join_time():
+    count = 0
+    output_file = open("in-game-logs.csv", "w")
+    data_writer = csv.writer(output_file)
+    in_game_transaction_hashes = set()
+    with open(per_user_join_time_json_file_path, "r") as per_user_join_time_json_file:
+        per_user_join_time = json.load(per_user_join_time_json_file)
+
+    with open(daily_transfers_with_timestamp_file_path, "r") as daily_transfers_csv_file:
+        data_reader = csv.reader(daily_transfers_csv_file)
+        next(data_reader)
+        for row in data_reader:
+            buyer_address = row[1]
+            seller_address = row[2]
+            value = float(row[3])
+            transaction_hash = row[4]
+            block_number = row[6]
+            timestamp = row[7]
+
+            if buyer_address in per_user_join_time and per_user_join_time[buyer_address]['timestamp'] < timestamp:
+                continue
+
+            per_user_join_time[buyer_address] = {
+                'timestamp': timestamp,
+                'transaction_hash': transaction_hash
+            }
+
+            if seller_address in per_user_join_time and per_user_join_time[seller_address]['timestamp'] < timestamp:
+                continue
+
+            per_user_join_time[seller_address] = {
+                'timestamp': timestamp,
+                'transaction_hash': transaction_hash
+            }
+
+    with open(per_user_join_time_json_file_path, "w") as per_user_join_time_json_file:
+        per_user_join_time_json_file.write(json.dumps(per_user_join_time))
+
+def main():
+    add_new_user_join_time()
+
+if __name__ == "__main__":
+    main()
