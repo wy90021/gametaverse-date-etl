4fe850a9cf8e54b6999d499da6311bd14f595d04
danlei <danleizhousf@gmail.com>
2022-02-13 23:54:12 -0800

    save snapshot and rm transactions after filtering

--- etl-script.sh
+++ etl-script.sh
@@ -14,12 +14,19 @@ range=$(ethereumetl get_block_range_for_date -d $1 --provider-uri https://bsc-da
 rangeArr=(${range//,/ })
 echo "Block range for $1: $range"
 echo "Env: ${env}"
+mkdir $1
 touch $1/blockrange-${rangeArr[0]}-${rangeArr[1]}.csv
 ethereumetl export_blocks_and_transactions --start-block ${rangeArr[0]} --end-block ${rangeArr[1]} --blocks-output $1/blocks.csv --transactions-output $1/transactions.csv --provider-uri https://bsc-dataseed.binance.org/ --max-workers 5 --batch-size 100 
 
 # filter transaction by game contracts, output in-game-transaction-hashes.csv
 echo "Get Transaction IDs"
 python3 get_game_transactions.py $1 $1/transactions.csv
+
+# Clean up transactions.csv to save disk space
+head -n 2 $1/transactions.csv > $1/transaction-snapshot.csv
+tail -n 2 $1/transactions.csv >> $1/transaction-snapshot.csv
+
+rm $1/transactions.csv
 
 ethereumetl export_receipts_and_logs --transaction-hashes $1/in-game-transaction-hashes.csv --logs-output $1/in-game-logs.csv --provider-uri https://bsc-dataseed.binance.org/ --max-workers 5 --batch-size 100
 ethereumetl extract_token_transfers --logs $1/in-game-logs.csv --output $1/in-game-token-transfers.csv
