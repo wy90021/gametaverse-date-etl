89e2a4ae22702b75edb9ad19db48820fad0f9547
danlei <danleizhousf@gmail.com>
2022-02-21 15:11:06 -0800

    clean up

--- reset-user-transfer-value.sh
+++ reset-user-transfer-value.sh
@@ -4,4 +4,3 @@ while [ "$d" != 2022-01-21 ]; do
   python3 reset_user_transactions_value.py --env prod $d/in-game-token-transfers.csv $d/blocks.csv
   d=$(date -I -d "$d + 1 day")
 done
-  # python3 upload_user_transactions.py --env prod 2022-02-16/in-game-token-transfers.csv 2022-02-16/blocks.csv
