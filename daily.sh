90d39806eb0e86d6d37f13ba00b44f23e2e4984d
danlei <danleizhousf@gmail.com>
2022-03-05 15:10:55 -0800

    add upload price history script

--- daily.sh
+++ daily.sh
@@ -8,5 +8,5 @@ d=$1
 bash block-script.sh $d
 bash etl-script.sh $d
 python3 daily-agg/process.py $d
-bash upload_s3.sh $d gametaverse-starsharks-daily preprocessed
-# bash ~/gametaverse-date-etl/dynamo-scripts.sh $d prod
+bash upload_price_history.sh
+bash upload_s3.sh $d gametaverse-daily/starsharks preprocessed
