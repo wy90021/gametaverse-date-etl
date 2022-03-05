48563c4bc8bb447f2a8483e6cf6e7d94e1e2ddd4
wy90021 <wy90021@gmail.com>
2022-03-04 22:50:13 -0800

    update

--- daily.sh
+++ daily.sh
@@ -1,12 +1,13 @@
-if [ -z "$1" ]
-then
-      echo "need starting date input, e.g. bash daily.sh 2021-12-20"
-      exit 0
-fi
-d=$1
+#if [ -z "$1" ]
+#then
+#      echo "need starting date input, e.g. bash daily.sh 2021-12-20"
+#      exit 0
+#fi
+#d=$1
+d=$(date +%F)
 
-bash block-script.sh $d
-bash etl-script.sh $d
-python3 daily-agg/process.py $d
+bash block-script.sh $d &&
+bash etl-script.sh $d &&
+python3 daily-agg/process.py $d &&
 bash upload_s3.sh $d gametaverse-starsharks-daily preprocessed
 # bash ~/gametaverse-date-etl/dynamo-scripts.sh $d prod
