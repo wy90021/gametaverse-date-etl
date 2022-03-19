d470a94103613076c7233db23229f189868ef144
wy90021 <wy90021@gmail.com>
2022-03-18 19:13:06 -0700

    update

--- daily.sh
+++ daily.sh
@@ -3,5 +3,5 @@ bash block-script.sh $d &&
 bash etl-script.sh $d &&
 python3 daily-agg/process.py $d &&
 bash upload_price_history.sh &&
-bash upload_s3.sh $d gametaverse-daily/starsharks preprocessed
+bash upload_s3.sh $d gametaverse-daily/starsharks preprocessed &&
 bash preload_cache.sh
