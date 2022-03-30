076a3d36d18ecdb9c57bd97de5b1a012300d8000
danlei <danleizhousf@gmail.com>
2022-03-29 21:13:27 -0700

    update

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -10,4 +10,5 @@ bash scripts/etl-script.sh $d &&
 python3 daily-agg/process.py $d &&
 bash scripts/upload_price_history.sh &&
 bash scripts/upload_s3.sh $d gametaverse-daily-starsharks preprocessed &&
+bash scripts/clear_cache.sh $d
 bash scripts/preload_cache.sh $d
